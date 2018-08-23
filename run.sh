08d944a0338b7d8014d8598e120d650e947fdd10
t <t@ts-MacBook.local>
2018-08-23 09:33:47 -0400

    added the python version

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+# script uses python 3
 #
 python3 ./src/pharmacy_sales_summary.py ./input/de_cc_data.txt ./output/top_cost_drug.txt
